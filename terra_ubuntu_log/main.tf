9d8a51ef131529db875fe629e78a650f74ac4179
root <root@qa-app-server.asia-south1-c.c.dev-environment-263212.internal>
2021-04-20 07:37:44 +0000

    new

--- terra_ubuntu_log/main.tf
+++ terra_ubuntu_log/main.tf
@@ -1,8 +1,9 @@
 // An Azure resource group
+
 resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_dns_support = true
 }
 
 resource "aws_internet_gateway" "gw" {
@@ -18,69 +19,72 @@ resource "aws_default_route_table" "route_table" {
 }
 
 resource "aws_security_group" "ingress-all" {
-  name   = "allow-all-sg"
+  name = "allow-all-sg"
   vpc_id = aws_vpc.vpc.id
   ingress {
     cidr_blocks = [
       "0.0.0.0/0"
     ]
     from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    to_port = 22
+    protocol = "tcp"
   }
   // Terraform removes the default rule
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 }
 
 resource "aws_subnet" "subnet1" {
-  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 3, 1)
-  vpc_id            = aws_vpc.vpc.id
-  availability_zone = var.aws_availabilityzone
+  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 3, 1)
+  vpc_id = aws_vpc.vpc.id
+  availability_zone = var.aws_availability_zone
 }
 
 data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    name = "name"
+    values = [
+      "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    name = "virtualization-type"
+    values = [
+      "hvm"]
   }
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_key_pair" "keypair" {
-  public_key = file("~/.ssh/id_rsa.pub")
+  owners = [
+    "099720109477"]
+  # Canonical
 }
 
 resource "aws_instance" "default" {
-  ami                         = data.aws_ami.ubuntu.id
+  ami = data.aws_ami.ubuntu.id
   associate_public_ip_address = true
-  instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.keypair.id
-  vpc_security_group_ids      = [aws_security_group.ingress-all.id]
-  subnet_id                   = aws_subnet.subnet1.id
-  user_data                   = data.template_file.user_data.rendered
+  instance_type = var.instance_type
+  key_name = var.key_name
+  vpc_security_group_ids = [
+    aws_security_group.ingress-all.id]
+  subnet_id = aws_subnet.subnet1.id
+  user_data = data.template_file.user_data.rendered
   tags = {
     Name = var.hostname
   }
 
   connection {
-    user        = "ubuntu"
-    private_key = file("~/.ssh/id_rsa")
-    agent       = false
-    host        = aws_instance.default.public_ip
+    user = "ubuntu"
+    password = var.password
+    //private_key = var.key_name
+    agent = false
+    host = aws_instance.default.public_ip
   }
 
   provisioner "file" {
-    source      = "scripts/install_arc_agent.sh"
+    source = "install_arc_agent.sh"
     destination = "/tmp/install_arc_agent.sh"
   }
   provisioner "remote-exec" {
@@ -91,7 +95,7 @@ resource "aws_instance" "default" {
     ]
   }
   provisioner "file" {
-    source      = "scripts/log_analytics_agent.sh"
+    source = "log_analytics_agent.sh"
     destination = "/tmp/log_analytics_agent.sh"
   }
   provisioner "remote-exec" {
@@ -104,32 +108,34 @@ resource "aws_instance" "default" {
 }
 
 resource "local_file" "install_arc_agent_sh" {
-  content = templatefile("scripts/install_arc_agent.sh.tmpl", {
+  content = templatefile("install_arc_agent.sh.tmpl", {
     resourceGroup = var.azure_resource_group
-    location      = var.azure_location
-    client_id     = var.client_id
+    location = var.azure_location
+    client_id = var.client_id
     client_secret = var.client_secret
-    tenant_id     = var.tenant_id
+    tenant_id = var.tenant_id
     subscription_id = var.subscription_id
-    }
+  }
   )
-  filename = "scripts/install_arc_agent.sh"
+  filename = "install_arc_agent.sh"
 }
 
 resource "local_file" "log_analytics_agent_sh" {
-  content = templatefile("scripts/log_analytics_agent.sh.tmpl", {
-    Workspace_Id       = var.Workspace_Id
-    Workspace_Key      = var.Workspace_Key
-    }
+  content = templatefile("log_analytics_agent.sh.tmpl", {
+    Workspace_Id = var.Workspace_Id
+    Workspace_Key = var.Workspace_Key
+  }
   )
-  filename = "scripts/log_analytics_agent.sh"
+  filename = "log_analytics_agent.sh"
 }
 
 
 data "template_file" "user_data" {
-  template = templatefile("scripts/user_data.tmpl", {
+  template = templatefile("user_data.tmpl", {
     hostname = var.hostname
-    }
+    username = "ubuntu"
+    password = var.password
+  }
   )
 }
 
